619f19ccfd29777eff71170830ad86866e433d98
Golobon <golobon@ya.ru>
2024-03-12 20:00:01 +0300

    Commit

--- app/src/main/java/com/golobon/gchat/MainActivity.java
+++ app/src/main/java/com/golobon/gchat/MainActivity.java
@@ -1,10 +1,22 @@
 package com.golobon.gchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    ImageButton iBtnSearchPerson;
+    ChatFragment chatFragment;
+    ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +24,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         UiStyleSettings.setUiStyle(this);
+
+        chatFragment = new ChatFragment();
+        profileFragment = new ProfileFragment();
+
+        bottomNavigationView = findViewById(R.id.main_bottom_navi_view);
+        iBtnSearchPerson = findViewById(R.id.btn_main_search_person);
+        iBtnSearchPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,
+                        SearchUserActivity.class));
+            }
+        });
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.menu_chat) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fr_layout_main, chatFragment)
+                            .commit();
+                }
+                else if (item.getItemId() == R.id.menu_profile) {
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fr_layout_main, profileFragment)
+                            .commit();
+                }
+                return true;
+            }
+        });
+
+        bottomNavigationView.setSelectedItemId(R.id.menu_chat);
     }
 }
