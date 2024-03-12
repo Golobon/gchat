c75754035fdd4a7a31a9b891bd328f63a7a1ce81
Golobon <golobon@ya.ru>
2024-03-12 21:17:44 +0300

    Commit

--- app/src/main/java/com/golobon/gchat/SearchUserActivity.java
+++ app/src/main/java/com/golobon/gchat/SearchUserActivity.java
@@ -1,10 +1,19 @@
 package com.golobon.gchat;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class SearchUserActivity extends AppCompatActivity {
+    EditText etSearchUserInput;
+    ImageButton btnSearchUser;
+    ImageButton btnBack;
+    RecyclerView rvFoundUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +21,27 @@ public class SearchUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search_user);
 
         UiStyleSettings.setUiStyle(this);
+
+        etSearchUserInput = findViewById(R.id.et_search_username_input);
+        btnSearchUser = findViewById(R.id.btn_search_user);
+        btnBack = findViewById(R.id.btn_search_back);
+        rvFoundUsers = findViewById(R.id.rv_search_user);
+
+        etSearchUserInput.requestFocus();
+
+        btnBack.setOnClickListener(v -> onBackPressed());
+
+        btnSearchUser.setOnClickListener(v -> {
+            String searchUserInfo = etSearchUserInput.getText().toString();
+            if (searchUserInfo.isEmpty() || searchUserInfo.length() < 3) {
+                etSearchUserInput.setError("Некорректное имя");
+                return;
+            }
+            setupSearchRecyclerView(searchUserInfo);
+        });
+    }
+
+    private void setupSearchRecyclerView(String searchUserInfo) {
+
     }
 }
